49c492748267c4a255bd6850646e8db71353f56a
Justin McLain <70154173+JustinMcLain1@users.noreply.github.com>
2026-05-04 19:28:31 -0500

    Add: number-of-students-unable-to-eat-lunch - submission-0

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        count = Counter(students) #{"1" : 2, "0": 2} depends on input
+        leftOver = len(students) #4
+
+        for sandwich in sandwiches:
+            if count[sandwich] > 0: 
+                count[sandwich] -=1
+                leftOver -= 1
+            else:
+                break
+        
+        return leftOver
