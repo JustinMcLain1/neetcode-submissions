d9e91aa2c5a2be81b93dbf26d3186b31c5f9e395
Justin McLain <70154173+JustinMcLain1@users.noreply.github.com>
2026-05-08 20:40:02 -0500

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        i =0 
+        a = 1
+        b = 2
+
+        if n <= 1:
+            return 1
+
+        for i in range(3, n + 1):
+            current = a + b
+            a = b
+            b = current
+        return b
